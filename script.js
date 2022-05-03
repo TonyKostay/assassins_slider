6cace24c15c60677f2f10ed266ac42f979d7bea8
TonyKostay <tonykostay@yandex.ru>
2022-05-03 21:17:17 +0300

    'completed_v1'

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+    const showSlider = new Swiper('.showcase__carousel',{
+        loop: true,
+        slidesPerView: 3,
+        speed: 1800, 
+        centeredSlides: true
+    });
+ });
